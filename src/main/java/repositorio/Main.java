c2463ab04069c00d18cc8bcc9c2a9d1ddaf0b8a4
rober <rober@10.0.9.14>
2021-09-13 19:06:58 -0500

    C1: Carga Inicial de Archivos al Repositorio 09/13/2021

---
+++ src/main/java/repositorio/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repositorio;
+
+/**
+ *
+ * @author rober
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Buenas Noches compañeros de Consultorio Médico Virtual");
+        System.out.println("Bienvenidos al repositorio!!");        
+                
+    }
+}
